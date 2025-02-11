6ac956f0e53e1fd3adec5ea69c17bc1015fca93f
François Colas <francoiscolas@gmail.com>
2025-02-11 11:24:05 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import Typewriter from "typewriter-effect/dist/core";
+
+(function () {
+    (new Typewriter("#code"))
+      .typeString("<strong>$</strong> ")
+      .pauseFor(1000)
+      .typeString("gcc -x c - &lt;&ltEOF<br/>")
+      .typeString("#include &lt;unistd.h&gt;<br/>")
+      .typeString("<br/>")
+      .typeString("int main()<br/>")
+      .typeString("{<br/>")
+      .typeString("&nbsp;&nbsp;&nbsp;&nbsp;write(1, \"Hi!\\n\", 4);<br/>")
+      .typeString("&nbsp;&nbsp;&nbsp;&nbsp;return 0;<br/>")
+      .typeString("}<br/>")
+      .typeString("EOF<br/>")
+      .pauseFor(1000)
+      .typeString("<strong>$</strong> ")
+      .pauseFor(1000)
+      .typeString("./a.out<br/>")
+      .typeString("Hi!<br/>")
+      .typeString("<strong>$</strong> ")
+      .start();
+})();
